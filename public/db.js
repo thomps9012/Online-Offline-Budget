e7864e8360abb52b8c4f8a2d9db4f666487a2516
Samuel Thompson <thomps9012@gmail.com>
2020-06-27 08:22:53 -0400

    updated saveRecord func

--- public/db.js
+++ public/db.js
@@ -19,11 +19,11 @@ request.onerror = function(event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
-function saveRecord(record) {
+function saveRecord(transaction) {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
 
-  store.add(record);
+  store.add(transaction);
 }
 
 function checkDatabase() {
